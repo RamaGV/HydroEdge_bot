22eddbfb8502025be679cfff80693a1747a4962f
RamaGV <ramagv23@gmail.com>
2024-10-19 16:49:24 -0300

    Actualizo
    Former-commit-id: 7432a155d893e663770a8f130d9553495730a051

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 # main
 from driver_setup import configurar_driver
-from whatsapp.whatsapp import WhatsAppWeb
 from db_manager import cargar_contacto
-from whatsapp.ui import WhatsAppUI
 from whatsapp.contacto import Contacto
-import time
-import logging
 from whatsapp.maquina_estados import Estado
+import utils as ut
 
 INIT = Estado.INIT
 STAND_BY = Estado.STAND_BY
@@ -14,8 +11,15 @@ PROCESAR_PANEL = Estado.PROCESAR_PANEL
 AGREGAR_CONTACTOS = Estado.AGREGAR_CONTACTOS
 ACTUALIZAR_CONTACTOS = Estado.ACTUALIZAR_CONTACTOS
 
+# Cargar el modelo Whisper
+
+# Transcribir todos los archivos de audio en el directorio especificado
+directory_historiales = "C:/Users/ramag/OneDrive/Desktop/HydroEdge/HydroEdge_bot/data/historiales"
+#model_whisper = wis.load_whisper_model("small")
 
 def inicializar():
+    pass
+    """
     representante = cargar_contacto("Gregorio")
     if not representante or not isinstance(representante, Contacto):
         logging.error("No se pudo encontrar el representante 'Gregorio' o no es un Contacto válido. Terminando el proceso.")
@@ -24,10 +28,16 @@ def inicializar():
     ui = WhatsAppUI(whatsapp)
     ui.abrir_whatsapp_web()
     time.sleep(2)
-    return whatsapp, True
-
+    return whatsapp, True"""
 
 def main():
+
+    #ut.procesar_archivos(directory_historiales)
+    text_file_path = 'C:/Users/ramag/OneDrive/Desktop/HydroEdge/HydroEdge_bot/data/historiales/+598 98 453 709/Chat de WhatsApp con +598 98 453 709.txt'
+    ut.procesar_historial_chat(text_file_path)
+
+    
+    """
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
@@ -64,7 +74,7 @@ def main():
         logging.error(f"Se produjo un error en el script: {e}")
     finally:
         if whatsapp:
-            whatsapp.driver.quit()
+            whatsapp.driver.quit()"""
 
 
 if __name__ == "__main__":
--- whisper/main.py
+++ whisper/main.py
@@ -1,10 +1,6 @@
 # main.py
-import os
-import tkinter as tk
-import sounddevice as sd
-import numpy as np
 import whisper_loader
-import scipy.io.wavfile as wav
+import os
 
 # Cargar el modelo Whisper
 model = whisper_loader.load_whisper_model("small")
@@ -13,30 +9,6 @@ model = whisper_loader.load_whisper_model("small")
 is_recording = False
 recording = None
 sample_rate = 44100
-
-def record_audio():
-    global is_recording, recording
-    duration = 5  # Duración inicial de la grabación en segundos (puede cambiarse)
-    print("Recording...")
-    recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
-    is_recording = True
-    sd.wait()  # Espera a que termine la grabación si se detiene manualmente
-    save_audio()
-
-def stop_recording():
-    global is_recording
-    if is_recording:
-        sd.stop()
-        is_recording = False
-        print("Recording stopped.")
-        save_audio()
-
-def save_audio():
-    # Guardar el audio en la misma carpeta que main.py
-    output_path = os.path.join(os.path.dirname(__file__), "recorded_audio.wav")
-    wav.write(output_path, sample_rate, np.array(recording))
-    print(f"Recording saved at {output_path}.")
-    transcribe_audio(output_path)
 
 def transcribe_audio(audio_path):
     text = whisper_loader.transcribe_audio(model, audio_path)
@@ -49,18 +21,3 @@ def save_transcription(text):
     with open(transcription_path, "w", encoding="utf-8") as file:
         file.write(text)
     print(f"Transcription saved at {transcription_path}.")
-
-# Interfaz gráfica
-root = tk.Tk()
-root.title("Whisper Audio Recorder")
-
-# Botón para empezar la grabación
-record_button = tk.Button(root, text="Start Recording", command=record_audio)
-record_button.pack(pady=10)
-
-# Botón para detener la grabación
-stop_button = tk.Button(root, text="Stop Recording", command=stop_recording)
-stop_button.pack(pady=10)
-
-# Iniciar la interfaz gráfica
-root.mainloop()
